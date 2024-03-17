eea0b9f9d24ecb9a7aa2247b391f4ce460ddad67
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-03-17 16:53:38 -0400

    lnwallet+channeldb: add new AuxLeafStore for dynamic aux leaves
    In this commit, we add a new AuxLeafStore which can be used to dynamically fetch the latest aux leaves for a given state. This is useful for custom channel types that will store some extra information in the form of a custom blob, then will use that information to derive the new leaf tapscript leaves that may be attached to reach state.

--- contractcourt/chain_watcher.go
+++ contractcourt/chain_watcher.go
@@ -433,7 +433,7 @@ func (c *chainWatcher) handleUnknownLocalState(
 	}
 	remoteScript, _, err := lnwallet.CommitScriptToRemote(
 		c.cfg.chanState.ChanType, c.cfg.chanState.IsInitiator,
-		commitKeyRing.ToRemoteKey, leaseExpiry,
+		commitKeyRing.ToRemoteKey, leaseExpiry, input.NoneTapLeaf(),
 	)
 	if err != nil {
 		return false, err
@@ -446,6 +446,7 @@ func (c *chainWatcher) handleUnknownLocalState(
 		c.cfg.chanState.ChanType, c.cfg.chanState.IsInitiator,
 		commitKeyRing.ToLocalKey, commitKeyRing.RevocationKey,
 		uint32(c.cfg.chanState.LocalChanCfg.CsvDelay), leaseExpiry,
+		input.NoneTapLeaf(),
 	)
 	if err != nil {
 		return false, err
