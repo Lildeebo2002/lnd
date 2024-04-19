792be6b744d2bd0918bbcf3a4fa1ac4b1ba0f147
George Tsagkarelis <george.tsagkarelis@gmail.com>
2024-04-19 14:00:34 +0200

    lnd: use impl cfg TlvTrafficShaper

--- server.go
+++ server.go
@@ -1018,6 +1018,7 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 		PathFindingConfig:  pathFindingConfig,
 		Clock:              clock.NewDefaultClock(),
 		ApplyChannelUpdate: s.graphBuilder.ApplyChannelUpdate,
+		TrafficShaper:      implCfg.TrafficShaper,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("can't create router: %w", err)
